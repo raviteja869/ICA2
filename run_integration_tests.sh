2767c97c2d2e542ad0d96b6b75e05a3a43ea2af7
raviteja8698 <raviteja.gannamaneni@gmail.com>
2023-11-19 00:06:58 -0500

    Add executable integration test script

---
+++ run_integration_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Running Integration Tests..."
+
+# Example command to run integration tests
+# Replace this with the actual command you use for integration testing
+curl http://localhost:5000/ # Example endpoint test
+
+# Add more commands as needed for different test cases

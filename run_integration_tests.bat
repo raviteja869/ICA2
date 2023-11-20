a0af782c2ab12f349287a189e0b3ad364ec0daa6
Ravi Teja Gannamaneni <132315611+raviteja869@users.noreply.github.com>
2023-11-19 22:23:39 -0500

    Update run_integration_tests.bat

--- run_integration_tests.bat
+++ run_integration_tests.bat
@@ -1,11 +1,8 @@
 @echo off
 
-REM Simple script to test the Flask application endpoints
+REM Simple script to test the Flask application's home route
 
-echo Running integration tests...
-
-REM Wait for a brief period to ensure the Flask server is up
-ping localhost -n 6 > nul
+echo Running integration test...
 
 REM Test the home route
 curl -f http://localhost:5000/
@@ -14,13 +11,4 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-REM Test the plot route
-curl -f -X POST -H "Content-Type: application/json" ^
-     -d "{\"xmin\": -2.0, \"xmax\": 1.0, \"xmin\": -1.5, \"ymax\": 1.5}" ^
-     http://localhost:5000/plot
-if %errorlevel% neq 0 (
-    echo Plot route test failed
-    exit /b 1
-)
-
-echo Integration tests passed successfully
+echo Integration test passed successfully

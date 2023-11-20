3359b67cb3dfc9dcee98062d6789416b046df6cf
Ravi Teja Gannamaneni <132315611+raviteja869@users.noreply.github.com>
2023-11-19 22:50:59 -0500

    Update deploy.sh

--- .github/actions/deploy-action/deploy.sh
+++ .github/actions/deploy-action/deploy.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
-# Deployment environment (production, staging, etc.)
 environment=$1
 
-# Server details
-SERVER_USER="your_username"
-SERVER_IP="your_server_ip"
-APP_DIRECTORY="/path/to/your/app/directory"
-GIT_REPOSITORY="https://github.com/raviteja869/ICA2"
+echo "Deploying to ${environment} environment..."
 
-echo "Starting deployment to ${environment}..."
-
-# SSH into the server and execute deployment commands
-ssh $SERVER_USER@$SERVER_IP << EOF
-
-    # Navigate to the application directory
-    cd $APP_DIRECTORY
-
-    # Pull the latest code
-    git pull $GIT_REPOSITORY
-
-    # Install any dependencies
-    # For Python projects, this might be:
-    pip install -r requirements.txt
-
-    # Restart the application
-    # This will vary greatly depending on how your application is set up.
-    # For a Flask app, you might use gunicorn, for example:
-    # pkill gunicorn
-    # nohup gunicorn -w 4 app:app &
-
-EOF
+# Add your deployment logic here
+# Example: SSH into a server and pull the latest Docker image
+# ssh user@server "docker pull ghcr.io/raviteja869/mandelbrot-set-visualizer:latest && docker-compose up -d"
 
 echo "Deployment to ${environment} completed."

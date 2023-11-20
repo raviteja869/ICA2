b74f8cf164a2a811f55d4fd2888e30408e4b384e
Ravi Teja Gannamaneni <132315611+raviteja869@users.noreply.github.com>
2023-11-19 20:35:42 -0500

    Update run_integration_tests.sh

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-echo "Running Integration Tests..."
+# Simple script to test the Flask application endpoints
 
-# Example command to run integration tests
-# Replace this with the actual command you use for integration testing
-curl http://localhost:5000/ # Example endpoint test
+echo "Running integration tests..."
 
-# Add more commands as needed for different test cases
+# Test the home route
+curl -f http://localhost:5000/
+if [ $? -ne 0 ]; then
+    echo "Home route test failed"
+    exit 1
+fi
+
+# Test the plot route
+curl -f -X POST -H "Content-Type: application/json" \
+     -d '{"xmin": -2.0, "xmax": 1.0, "ymin": -1.5, "ymax": 1.5}' \
+     http://localhost:5000/plot
+if [ $? -ne 0 ]; then
+    echo "Plot route test failed"
+    exit 1
+fi
+
+echo "Integration tests passed successfully"

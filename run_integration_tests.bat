f761b8dbe19eff253f21284666c5f9e1b3ea0415
Ravi Teja Gannamaneni <132315611+raviteja869@users.noreply.github.com>
2023-11-19 22:13:56 -0500

    Update run_integration_tests.bat

--- run_integration_tests.bat
+++ run_integration_tests.bat
@@ -4,6 +4,9 @@ REM Simple script to test the Flask application endpoints
 
 echo Running integration tests...
 
+REM Wait for a brief period to ensure the Flask server is up
+ping localhost -n 6 > nul
+
 REM Test the home route
 curl -f http://localhost:5000/
 if %errorlevel% neq 0 (
@@ -13,7 +16,7 @@ if %errorlevel% neq 0 (
 
 REM Test the plot route
 curl -f -X POST -H "Content-Type: application/json" ^
-     -d "{\"xmin\": -2.0, \"xmax\": 1.0, \"ymin\": -1.5, \"ymax\": 1.5}" ^
+     -d "{\"xmin\": -2.0, \"xmax\": 1.0, \"xmin\": -1.5, \"ymax\": 1.5}" ^
      http://localhost:5000/plot
 if %errorlevel% neq 0 (
     echo Plot route test failed

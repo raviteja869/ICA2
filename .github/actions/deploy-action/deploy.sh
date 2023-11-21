f8f4c0494a2f4dd65828d61533bd928744bed631
Ravi Teja Gannamaneni <132315611+raviteja869@users.noreply.github.com>
2023-11-21 01:43:32 -0500

    Update deploy.sh

--- .github/actions/deploy-action/deploy.sh
+++ .github/actions/deploy-action/deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Deployment script for the Mandelbrot Set Visualizer application
+
+# Arguments
 environment=$1
 
+# Deployment logic
 echo "Deploying to ${environment} environment..."
 
-# Add your deployment logic here
-# Example: SSH into a server and pull the latest Docker image
-# ssh user@server "docker pull ghcr.io/raviteja869/mandelbrot-set-visualizer:latest && docker-compose up -d"
+# SSH command to pull the latest Docker image and restart the service using Docker Compose
+# Replace 'user@server' with your server's SSH login and 'path_to_docker_compose' with the actual path
+ssh user@server "cd path_to_docker_compose && \
+                docker pull ghcr.io/raviteja869/mandelbrot-set-visualizer:latest && \
+                docker-compose down && \
+                docker-compose up -d"
 
-echo "Deployment to ${environment} completed."
+echo "Deployment to ${environment} environment completed."
